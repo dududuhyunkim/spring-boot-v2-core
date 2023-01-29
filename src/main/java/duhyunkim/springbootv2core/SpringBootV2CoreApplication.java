68477e271d6cb802dfe1d1385bce746ed3eb83bc
duhyunkim <duhyun.dev@gmail.com>
2023-01-29 18:27:04 +0900

    Initial commit

---
+++ src/main/java/duhyunkim/springbootv2core/SpringBootV2CoreApplication.java
@@ -0,0 +1,13 @@
+package duhyunkim.springbootv2core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootV2CoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootV2CoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/duhyunkim/springbootv2core/SpringBootV2CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package duhyunkim.springbootv2core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootV2CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
